5b7910bebc512de0d25c056a8d89a34b1999ace2
BSF <bsf@Akashs-Macbook-Air.local>
2024-02-15 11:50:48 +0530

    refactor

---
+++ types/index.ts
@@ -0,0 +1,8 @@
+export type ChipItem = {
+  label: string;
+  value: string;
+};
+
+export type ChipsType = "default" | "filter" | "input";
+
+export type ChipItemVariant = "solid" | "outlined" | "disabled";
